23d58a74f5b3e69ac54f255ae33d236137fd3ecd
SamuelFDEZS <thggsamuel@gmail.com>
2023-10-24 00:20:25 +0200

    Added TicTacToe logic and functions for updating and resetting the board. Also created a winner variable to store the winner

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,73 @@ const TURNS = {
 
 import { useState } from "react"
 
-
 const Square = ({ children, isSelected, updateBoard, index }) => {
+  const handleClick = () => {
+    updateBoard(index);
+  }
   const className = `square ${isSelected ? 'is-selected' : ''}`
   return (
-    <div className={className}>
+    <div onClick={handleClick} className={className}>
       {children}
     </div>
   )
 }
+
+const WINNER_COMBOS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    for (let combo of WINNER_COMBOS) {
+      const [a,b,c] = combo
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ){
+      return boardToCheck[a]
+      }
+    }
+    //If no winner
+    return null
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.X)
+    setWinner(null)
+  }
+  const updateBoard = (index) => {
+    if (board[index] || winner) return
+    //We manage turns
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn)
+
+    //We update the array
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+    console.log(board)
+
+    const newWinner = checkWinner(newBoard)
+    setWinner(newWinner)
+  }
+
+
+
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
@@ -27,22 +82,43 @@ function App() {
               <Square
                 key={index}
                 index={index}
+                updateBoard={updateBoard}
               >
-              {board[index]}
+                {board[index]}
               </Square>
             )
           })
         }
       </section>
       <section className="turn">
-        <Square isSelected = {turn === TURNS.X}>
+        <Square isSelected={turn === TURNS.X}>
           {TURNS.X}
         </Square>
 
-        <Square isSelected = {turn === TURNS.O}>
+        <Square isSelected={turn === TURNS.O}>
           {TURNS.O}
         </Square>
       </section>
+
+      {
+        winner !== null && (
+          <section className="winner">
+          <div className="text">
+          <h2>{
+            winner === false ? "Draw" : `Winner: `
+          }</h2>
+
+          <header className="win">
+            {winner && <Square>{winner}</Square>}
+          </header>
+
+          <footer>
+            <button onClick={resetGame}>Play again</button>
+          </footer>
+          </div>
+          </section>
+        )
+      }
     </main>
 
   )
