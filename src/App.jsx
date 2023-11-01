463936bbfdc14df0cd03dbe39e0e845bacd51860
SamuelFDEZS <thggsamuel@gmail.com>
2023-11-01 18:05:02 +0100

    Relocated functions and logic for cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -1,101 +1,79 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
-import { Square } from "./components/Square.jsx"
-import { TURNS } from "./constants.js"
-import { checkWinner } from "./logic/board.js"
+import { Square } from "./components/Square.jsx";
+import { TURNS } from "./constants.js";
+import { checkWinner } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
 import { checkEndGame } from "./logic/board.js";
 
-
 function App() {
-  
   const [board, setBoard] = useState(() => {
-    const boardFromStorage = window.localStorage.getItem('board')
-    return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null)
+    const boardFromStorage = window.localStorage.getItem("board");
+    return boardFromStorage
+      ? JSON.parse(boardFromStorage)
+      : Array(9).fill(null);
   });
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ?? TURNS.X
+    const turnFromStorage = window.localStorage.getItem("turn");
+    return turnFromStorage ?? TURNS.X;
   });
-  const [winner, setWinner] = useState(null)
+  const [winner, setWinner] = useState(null);
 
   const resetGame = () => {
-    setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
+    setBoard(Array(9).fill(null));
+    setTurn(TURNS.X);
+    setWinner(null);
 
-    window.localStorage.removeItem('board')
-    window.localStorage.removeItem('turn')
-    
-  }
+    window.localStorage.removeItem("board");
+    window.localStorage.removeItem("turn");
+  };
 
-  
   const updateBoard = (index) => {
-    if (board[index] || winner) return
+    if (board[index] || winner) return;
     //We manage turns
     const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
-    setTurn(newTurn)
+    setTurn(newTurn);
 
     //We update the array
-    const newBoard = [...board]
-    newBoard[index] = turn
-    setBoard(newBoard)
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
 
-    window.localStorage.setItem('board', JSON.stringify(newBoard))
-    window.localStorage.setItem('turn', newTurn)
+    window.localStorage.setItem("board", JSON.stringify(newBoard));
+    window.localStorage.setItem("turn", newTurn);
 
-    const newWinner = checkWinner(newBoard)
+    const newWinner = checkWinner(newBoard);
 
     if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-
+      confetti();
+      setWinner(newWinner);
     } else if (checkEndGame(newBoard)) {
-      setWinner(false)
+      setWinner(false);
     }
-  }
-
-
-
+  };
 
   return (
-    <main className='board'>
+    <main className="board">
       <h1>Tic Tac Toe</h1>
       <button onClick={resetGame}>Reset game</button>
-      <section className='game'>
-        {
-          board.map((square, index) => {
-            return (
-              <Square
-                key={index}
-                index={index}
-                updateBoard={updateBoard}
-              >
-                {square}
-              </Square>
-            )
-          })
-        }
+      <section className="game">
+        {board.map((square, index) => {
+          return (
+            <Square key={index} index={index} updateBoard={updateBoard}>
+              {square}
+            </Square>
+          );
+        })}
       </section>
       <section className="turn">
-        <Square isSelected={turn === TURNS.X}>
-          {TURNS.X}
-        </Square>
+        <Square isSelected={turn === TURNS.X}>{TURNS.X}</Square>
 
-        <Square isSelected={turn === TURNS.O}>
-          {TURNS.O}
-        </Square>
+        <Square isSelected={turn === TURNS.O}>{TURNS.O}</Square>
       </section>
 
-      {
-        <WinnerModal 
-          winner={winner}
-          resetGame={resetGame}
-        />
-      }
+      {<WinnerModal winner={winner} resetGame={resetGame} />}
     </main>
-
-  )
+  );
 }
 
-export default App
+export default App;
